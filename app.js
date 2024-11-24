918f2de20e5b83058c56c7e62aa9f23cedfcdf69
krishnathapa43 <crissnathapa1020@gmail.com>
2024-11-24 23:45:40 +0545

    Day 5 create book , read all book , read single book(CRUD)

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose');
 const connectToDatabase = require('./Database');
+const Book = require('./model/bookModel');
 
-
+app.use(express.json())
 connectToDatabase()
 
 //Alternative
@@ -12,13 +13,48 @@ connectToDatabase()
 
 //json code
 app.get("/",(req,res)=>{
-    res.status(200
-        
-    ).json({
+    res.status(200).json({
         "name" : "krishna Bahadur Thapa",
         "age":22
     })
 })
+
+//Create book a API , create table book and connect with mongodb
+app.post("/book",async(req,res)=>{
+    const {bookName,bookPrice,isbnNumber,authorName,publishedAt,publication} = req.body
+    await Book.create({
+    bookName,
+    bookPrice,
+    isbnNumber,
+    authorName,
+    publishedAt,
+    publication
+   })
+   res.status(201).json({
+    message : "Book created Successfully"
+   })
+})
+
+//Read All Book 
+app.get("/book",async (req,res)=>{
+    const books =await Book.find()   //find = return array ma garxa
+    res.status(200).json({
+        message : "Books fetched successfully",
+        data :books
+    })
+})
+
+//Read single Book
+app.get("/book/:id",async (req,res)=>{
+    const id = req.params.id
+    const book =await Book.findById(id)  //findByid = return object garxa
+    console.log(book)
+    res.json({
+        message : "single Book Fetched Successfully",
+        date : book
+    })
+})
+
 
 
 app.listen(3000,()=> {
